b95ad6549d2d0ad5b7918f14bc5c5ba7c2ce07d6
alteregodev <alteregodev@users.noreply.github.com>
2025-08-25 16:29:29 +0200

    Initial commit

---
+++ bot/commands/commands.py
@@ -0,0 +1,76 @@
+import disnake
+import datetime
+import aiofiles
+from disnake.ext import commands
+
+def load(bot: commands.Bot):
+    async def get_error_embed(error: str):
+        embed = disnake.Embed(
+            title='Error',
+            description=error
+        )
+        return embed
+
+    @bot.event
+    async def on_ready():
+        current_time = datetime.datetime.now()
+
+        print(f'\nLogged in as {bot.user}')
+        print(f'Current time: {current_time.strftime('%Y-%m-%d %H:%M:%S')}')
+
+    @bot.slash_command(description='Check bot\'s latency')
+    async def ping(inter: disnake.ApplicationCommandInteraction):
+        embed = disnake.Embed(
+            title='Pong!',
+            description=f'Bot\'s latency - `{round(bot.latency * 1000, 2)}ms`'
+        )
+        await inter.response.send_message(embed=embed)
+
+    @bot.slash_command(description='Get information about a user')
+    async def user_info(inter: disnake.ApplicationCommandInteraction, user_id):
+        try:
+            user_id = int(user_id) # No, you can't just "user_id: int", because discord doesn't allow to pass big numbers as an argument for some reason
+        except TypeError:
+            error_embed = await get_error_embed('Please enter a valid id')
+            await inter.response.send_message(embed=error_embed, ephemeral=True)
+
+        try:
+            user = await bot.fetch_user(user_id)
+            embed = disnake.Embed(
+                title=f'Information about {user.name}',
+                description=f'''
+                    > User ID : `{user.id}`
+                    > User display name : `{user.display_name}`
+                    > Profile accent color : `{user.accent_color}`
+                    > User account created at : `{user.created_at.strftime('%Y-%m-%d %H:%M:%S')}`
+                '''
+            )
+            if user.avatar:
+                embed.set_thumbnail(url=user.avatar.url)
+
+            if user.banner:
+                embed.set_image(url=user.banner)
+
+            await inter.response.send_message(embed=embed)
+
+        except disnake.NotFound:
+            error_embed = await get_error_embed('Didn\'t find a user with such id')
+            await inter.response.send_message(embed=error_embed, ephemeral=True)
+
+    @bot.slash_command(description='Get information about the bot')
+    async def about(inter: disnake.ApplicationCommandInteraction):
+        async with aiofiles.open('.version', 'r') as f:
+            version = await f.read()
+
+        embed = disnake.Embed(
+            title=f'OsintCord {version}',
+            description='''
+                > Developer - [GitHub](https://github.com/alteregodev)
+                > Made with - `Python, Disnake, Love <3`
+                > Bot\'s GitHub - [GitHub Repo](https://github.com/alteregodev/OsintCord)
+                **🔎 Thank you for using OsintCord!**
+            '''
+        )
+        embed.set_thumbnail(url=bot.user.avatar.url)
+
+        await inter.response.send_message(embed=embed)
---
+++ bot/core.py
@@ -0,0 +1,21 @@
+import disnake
+import datetime
+from dotenv import load_dotenv
+from os import getenv
+from disnake.ext import commands
+
+from commands.commands import load
+
+def main():
+    load_dotenv()
+    BOT_TOKEN = getenv('BOT_TOKEN')
+
+    intents = disnake.Intents.default()
+    bot = commands.Bot(command_prefix='?', intents=intents) # The command prefix is unused, since this is a slash command bot
+
+    load(bot)
+    bot.run(BOT_TOKEN)
+
+if __name__ == '__main__':
+    main()
+
