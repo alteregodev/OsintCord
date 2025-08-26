1d111fe519cd8fd86f93c2a0f5ec5378fe349a14
alteregodev <alteregodev@users.noreply.github.com>
2025-08-26 11:40:45 +0200

    Added json functions to utils.py, fixed some bugs

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -1,3 +1,4 @@
+import json
 import disnake
 import aiofiles
 from disnake.ext import commands
@@ -10,8 +11,26 @@ async def get_error_embed(error: str):
         return embed
 
 async def get_file_content(filename):
-    async with aiofiles.open('.version', 'r') as f:
-        content = await f.read()
-    return content
+    try:
+        async with aiofiles.open(filename, 'r') as f:
+            content = await f.read()
+        return content
+    except Exception as e:
+        print('Error:', e)
 
+async def load_json_file(filename):
+    try:
+        async with aiofiles.open(filename, 'r') as f:
+            json_str = await f.read()
+            data = json.loads(json_str)
+        return data
+    except Exception as e:
+        print('Error:', e)
 
+async def dump_json_to_file(filename, data):
+    try:
+        async with aiofiles.open(filename, 'w') as f:
+            json_str = json.dumps(data, indent=4)
+            await f.write(json_str)
+    except Exception as e:
+        print('Error:', e)
