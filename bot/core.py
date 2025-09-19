6241fd509bff19383a13a4f4958b8efaf3b58765
alteregodev <alteregodev@users.noreply.github.com>
2025-09-19 14:45:18 +0200

    changed command prefix to silence the warning

--- bot/core.py
+++ bot/core.py
@@ -16,7 +16,7 @@ def main():
     BOT_TOKEN = getenv('BOT_TOKEN')
 
     intents = disnake.Intents.default()
-    bot = commands.Bot(command_prefix='?', intents=intents) # The command prefix is unused, since this is a slash command bot
+    bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents) # The command prefix is unused, since this is a slash command bot
 
     load(bot)
     bot.run(BOT_TOKEN)
